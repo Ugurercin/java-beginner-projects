9dc467eaa40fccb65396621eb28eb3189c729b94
Ugur <ercinugur@gmail.com>
2021-08-31 21:45:50 +0300

    main method modified for testing Hamburger.java

--- BurgerProject/Main.java
+++ BurgerProject/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        Hamburger hamburger = new Hamburger("Basic", "Sassuage", 3.56, "white");
+        hamburger.addHamburgerAddition1("Tomato", 2.56);
+        hamburger.addHamburgerAddition2("Lettuce", 1.56);
+        hamburger.itemizeHamburger();
     }
 }
