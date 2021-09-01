dd2f883064b026a3afe156a5c4ed1922f7098e9a
Ugur <ercinugur@gmail.com>
2021-09-01 21:45:08 +0300

    main method updated for hamburger.java

--- BurgerProject/Main.java
+++ BurgerProject/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Hamburger hamburger = new Hamburger("Basic", "Sassuage", 3.56, "white");
-        hamburger.addHamburgerAddition1("Tomato", 2.56);
-        hamburger.addHamburgerAddition2("Lettuce", 1.56);
+
+        Hamburger hamburger = new Hamburger("Basic", "sasuage", "white", 3.56);
+        hamburger.addHamburgerAddition1("Lettuce", 2.20);
+        hamburger.addHamburgerAddition2("Ketchup", 1);
+        hamburger.addHamburgerAddition3("Mayo", 2);
+        hamburger.addHamburgerAddition4("tomato", 2.5);
         hamburger.itemizeHamburger();
     }
 }
