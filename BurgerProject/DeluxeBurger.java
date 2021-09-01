ba7404ce77940535061b20ca65d491471526cd47
Ugur <ercinugur@gmail.com>
2021-09-01 21:58:02 +0300

    final version

--- BurgerProject/DeluxeBurger.java
+++ BurgerProject/DeluxeBurger.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class DeluxeBurger {
+public class DeluxeBurger extends Hamburger{
+    public DeluxeBurger() {
+        super("Deluxe", "Cheese & Bacon", "white", 6);
+        super.addHamburgerAddition1("Chips", 2.50);
+        super.addHamburgerAddition2("Cola", 1.50);
+        super.addHamburgerAddition3("Onion Rings", 5);
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("Adding an extra item is not allowed.");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("Adding an extra item is not allowed.");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("Adding an extra item is not allowed.");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("Adding an extra item is not allowed.");
+    }
 }
