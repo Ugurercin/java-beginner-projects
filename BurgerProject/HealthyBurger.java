5e2a52aaac8b6599462c333b2b76b5d8fb687b94
Ugur <ercinugur@gmail.com>
2021-09-01 22:12:30 +0300

    initial commit

---
+++ BurgerProject/HealthyBurger.java
@@ -0,0 +1,21 @@
+public class HealthyBurger extends Hamburger {
+    private String healthyExtra1Name;
+    private String healthyExtra2Name;
+    private double healthyExtra1Price;
+    private double healthyExtra2Price;
+
+    public HealthyBurger(String meat, double price) {
+        super("Healthy", meat, "Brown rye", price);
+    }
+
+    public void addHealthyAddition1(String name, double price){
+        this.healthyExtra1Name = name;
+        this.healthyExtra1Price = price;
+    }
+
+    public void addHealthyAddition2(String name, double price){
+        this.healthyExtra2Name = name;
+        this.healthyExtra2Price = price;
+    }
+
+}
