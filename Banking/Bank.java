676ed3f3376e502fe275c82576a868c1b49fa0e9
Ugur <ercinugur@gmail.com>
2021-09-18 22:49:43 +0300

    initial commit

--- Banking/Bank.java
+++ Banking/Bank.java
@@ -2,6 +2,19 @@ import java.util.ArrayList;
 
 public class Bank {
     private String bankName;
-
     private ArrayList<Customer> customers;
+    private ArrayList<Account> accounts;
+
+    public Bank(String bankName) {
+        this.bankName = bankName;
+        this.customers = new ArrayList<Customer>();
+        this.accounts = new ArrayList<Account>();
+    }
+
+
+
+
+
 }
+
+
